e16f15fc20131a0d83beb058a72afc50d69e29bc
Thanozio <thanozio@proton.me>
2025-03-18 18:35:56 +0200

    solve the 2 crystal balls problem

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,18 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const step = Math.floor(Math.sqrt(breaks.length));
+    let current = 0;
+    for (let i = 0; i < breaks.length; i += step) {
+        if (breaks[i]) {
+            current = i;
+            break;
+        }
+    }
 
+    for (let j = current - step; j <= current; j++) {
+        if (breaks[j]) {
+            return j;
+        }
+    }
+
+    return -1;
 }
