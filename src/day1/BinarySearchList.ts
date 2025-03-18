c31b2a25b62ba8c9f9a52954e53fe774ce1d1c7c
Thanozio <thanozio@proton.me>
2025-03-18 16:54:25 +0200

    complete search section

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,16 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
+    do {
+        const m = Math.floor(lo + (hi - lo) / 2);
+        if (haystack[m] === needle) {
+            return true;
+        } else if (haystack[m] < needle) {
+            lo = m + 1;
+        } else {
+            hi = m;
+        }
+    } while (lo < hi);
 
+    return false;
 }
