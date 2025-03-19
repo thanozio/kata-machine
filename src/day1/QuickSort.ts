6bef1b4d117f2debc765c8def5e8956394c5a336
Thanozio <thanozio@proton.me>
2025-03-19 21:12:39 +0200

    add quick sort

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,30 @@
-export default function quick_sort(arr: number[]): void {
 
+
+function qs(arr: number[], lo: number, hi: number): void {
+    if (lo < hi) {
+        const p = partition(arr, lo, hi);
+        qs(arr, lo, p-1);
+        qs(arr, p+1, hi);
+    }
+}
+
+// treating arr[hi] as pivot point
+function partition(arr: number[], lo: number, hi: number): number {
+    let idx = lo - 1;
+    for (let i = lo; i < hi; i++) {
+        if (arr[i] <= arr[hi]) {
+            idx++;
+            const tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+    }
+
+    idx++;
+    [arr[hi], arr[idx]] = [arr[idx], arr[hi]];
+    return idx;
+}
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
 }
