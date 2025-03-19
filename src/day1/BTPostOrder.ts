84fe00dee2165fde2bfe66dd9fce642862756b5b
Thanozio <thanozio@proton.me>
2025-03-19 22:36:19 +0200

    add Binary Search Tree traversals

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,16 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    if (!curr) {
+        return path;
+    }
 
+    return [
+        ...walk(curr.left, path),
+        ...walk(curr.right, path),
+        curr.value
+    ];
+}
+
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
