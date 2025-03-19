fc89771c00983ff191c37d2b6aa6c5cfd633bdd2
Thanozio <thanozio@proton.me>
2025-03-19 16:26:09 +0200

    add Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,35 @@
+type Node<T> = {
+    value: T,
+    prev?: Node<T>
+};
+
 export default class Stack<T> {
-    public length: number;
-
-    
-
-    constructor() {
-    }
+    public length: number = 0;
+    private head?: Node<T>;
 
     push(item: T): void {
+        this.length++;
+        const node: Node<T> = {
+            value: item,
+            prev: this.head
+        };
 
-}
+        this.head = node;
+    }
+
+
     pop(): T | undefined {
+        if (!this.head)
+            return;
 
-}
+        this.length--;
+        
+        const node = this.head;
+        this.head = this.head.prev;
+        return node.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
