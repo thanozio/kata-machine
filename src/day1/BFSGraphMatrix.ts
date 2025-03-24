e1dc15276d9d011e7ba742e1a7b325063cda7dac
Thanos Dimitriades <thanozio@proton.me>
2025-03-24 19:23:16 +0200

    add Breadth-First Search on graph with adjacency matrix

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,46 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+export default function bfs(
+    graph: WeightedAdjacencyMatrix, 
+    source: number, 
+    needle: number): number[] | null {
 
+        const seen = new Array(graph.length).fill(false);
+        const prev = new Array(graph.length).fill(-1);
+
+        seen[source] = true;
+        const q: number[] = [source];
+        do {
+            const curr = q.shift() as number;
+            if (curr === needle) {
+                break;
+            }
+
+            seen[curr] = true;
+            const adjacent = graph[curr];
+            for (let i = 0; i < adjacent.length; i++) {
+                if (adjacent[i] === 0 || seen[i]) {
+                    continue;
+                }
+
+                seen[i] = true;
+                prev[i] = curr;
+                q.push(i);
+            }
+
+        } while(q.length);
+
+
+        if (prev[needle] === -1) {
+            return null;
+        }
+
+        // build it backwards
+        let curr = needle;
+        const out: number[] = [];
+
+        while (prev[curr] !== -1) {
+            out.push(curr);
+            curr = prev[curr];
+        }
+
+        return [source].concat(out.reverse());
 }
