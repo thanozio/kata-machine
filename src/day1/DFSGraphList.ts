5917a4faf80a5707884be78a5edd263f626b9dc6
Thanos Dimitriades <thanozio@proton.me>
2025-03-24 19:48:07 +0200

    DFS Graph with Adjacency list

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,40 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+function walk(
+    graph: WeightedAdjacencyList, 
+    curr: number, 
+    needle: number, 
+    seen: boolean[], 
+    path: number[]): boolean {
+        if (seen[curr]) 
+            return false;
+        seen[curr] = true;
 
+        path.push(curr);
+        if (curr === needle) {
+            return true;
+        }
+
+        const edges = graph[curr];
+        for (let i = 0; i < edges.length; i++) {
+            const edge = edges[i];
+
+            if (walk(graph, edge.to, needle, seen, path)) {
+                return true;
+            }
+        }
+
+        path.pop();
+
+        return false;
+}
+
+export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+    const seen: boolean[] = new Array(graph.length).fill(false);
+    const path: number[] = [];
+    walk(graph, source, needle, seen, path);
+
+    if (path.length === 0) {
+        return null;
+    }
+
+    return path;
 }
